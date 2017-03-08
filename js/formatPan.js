bd9d3bf1f770e5299461872bdd63daf033320672
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2017-03-08 13:10:40 -0700

    initial

---
+++ js/formatPan.js
@@ -0,0 +1,50 @@
+/**
+ * @file formatPan-js
+ * Format PAN field by adding white space
+ */
+
+/**
+ * Return card type from pan
+ * @function detectCardType
+ * @param {String} number 
+ * @returns {Boolean}
+ */
+function detectCardTypePartial(pan) {
+  var re = {
+    //visa: 4
+    visa: /^4/,
+    //mastercard 50 - 55 OR 51-55?
+    mastercard: /^5[0-5]/,
+    //34, 37
+    amex: /^3[47]/,
+    //discover: 6011, 622126-622925, 644-649, 65
+    discover: /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)/
+  };
+
+  for(var key in re) {
+    if(re[key].test(pan)) {
+        return key;
+    }
+  }
+  return undefined;
+}
+
+function formatPan( pan ){
+  var type;
+  if ( (pan.length >= 2 || type == undefined) ){
+    type = detectCardTypePartial(pan);
+  }
+
+  return function(){
+    if (type == "amex"){
+      if (pan.length % 4 == 0 || pan.length % 11 == 0 ){
+        return pan + ' ';
+      }else return pan;
+    }
+    else{
+      if (pan.length % 4 == 0 || pan.length % 9 == 0 || pan.length % 14 == 0/* check pan size? */){
+        return pan + ' ';
+      }else return pan;
+    }
+  }
+}
