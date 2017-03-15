ada10c3da7944a1cac9f6d10eb9dce13f6750cab
Jeff Flower <jeffrey.flower@vantiv.com>
2017-03-15 10:38:36 -0600

    Validate expiry month and year separately
    Updated design needed validation to be separate

--- js/expiration-validation.js
+++ js/expiration-validation.js
@@ -1,33 +1,44 @@
 /**
  * @file expiration-validation.js
- * pure validation function@s for the expiration Input field
+ * pure validation functions for the expiration Input field
  */
 
- /*
+/*
  * Validations:
  * 1) Check if expiration date has passed
  */
 
 /**
- * Compare input with server month and year to see if the expiration date has passed
- * Current year and current month are considered valid (Caution: some cards expire at the beginning of the month)
- * @function validExpirationDate
- * @param {String} month - xx format
- * @param {String} year - xxxx format
- * @returns {Boolean}
+ * @function monthNotValid 
+ * @param {Int} month
+ * @param {Int} year
+ * @return {Boolean} false if year <= current year and month >= current month OR if year > current year, true otherwise 
  */
-function validExpirationDate(month, year){
-	//get current date
-	var today = new Date();
-	var currentMonth = today.getMonth() + 1; //month begins index at 0
-	var currentYear = today.getFullYear(); 
+function monthNotValid(month, year) {
+  //get current date
+  var today = new Date();
+  var currentMonth = today.getMonth() + 1; //month begins index at 0
+  var currentYear = today.getFullYear(); 
 
-	//if year has passed
-	if (year < currentYear){
-		return false;
-	}
-	//if month has passed in current year
-	else if( month < currentMonth && year == currentYear ){
-		return false;
-	}else return true;
+  if(year <= currentYear) {
+    return month < currentMonth;
+  }
+
+  if(year > currentYear) {
+    return false;
+  }
+
+}
+
+/**
+ * @function monthNotValid 
+ * @param {Int} month
+ * @return {Boolean} false if year >= current year, true otherwise
+ */
+function yearNotValid(year) {
+  // get current date
+  var today = new Date();
+  var currentYear = today.getFullYear(); 
+
+  return year < currentYear; 
 }
--- test/expiration-validation-test.js
+++ test/expiration-validation-test.js
@@ -2,38 +2,45 @@ var expect = chai.expect;
 
 
 var today = new Date();
-var validMonth = today.getMonth() + 1; //month starts index at zero
-var validYear = today.getFullYear();
+var currentMonth = today.getMonth() + 1; //month starts index at zero
+var currentYear = today.getFullYear();
+var futureYear = currentYear + 1;
 
 var pastMonth = today.getMonth();
 var pastYear = today.getFullYear() -1;
 
 describe('Expiration Date Validation', function() {  
-  describe('validExpirationDate', function() {
 
-    it('should return true given the current month and year', function() {
-      //expect(foo).to.equal('bar');
-      var testOne = validExpirationDate(validMonth, validYear);
-      expect(testOne).to.equal(true);
+  describe('monthNotValid', function() {
+    describe('year is less than or equal to the current year', function() {
+      it('returns false if input month >= current month', function() {
+        expect(monthNotValid(currentMonth, currentYear)).to.be.false;
+        expect(monthNotValid(currentMonth, pastYear)).to.be.false;
+      });
+
+      it('returns true if input month < current month', function() {
+        expect(monthNotValid(pastMonth, currentYear)).to.be.true;
+        expect(monthNotValid(pastMonth, pastYear)).to.be.true;
+      });
     });
 
-    it('should return false given a valid month and invalid year', function() {
-      //expect(foo).to.equal('bar');
-      var testTwo = validExpirationDate(validMonth, pastYear);
-      expect(testTwo).to.equal(false);
+    describe('year is greater than the current year', function() {
+      it('returns false for any input month', function() {
+        expect(monthNotValid(currentMonth, futureYear)).to.be.false;
+        expect(monthNotValid(pastMonth, futureYear)).to.be.false;
+      });
     });
-
-    it('should return false given an invalid month and valid year', function() {
-      //expect(foo).to.equal('bar');
-      var testThree = validExpirationDate(pastMonth, validYear);
-      expect(testThree).to.equal(false);
-    });
-
-    it('should return false given an invalid month and invalid year', function() {
-      //expect(foo).to.equal('bar');
-      var testFour = validExpirationDate(pastMonth, pastYear);
-      expect(testFour).to.equal(false);
-    });
-
   });
+
+  describe('yearNotValid', function() {
+    it('returns false if input year >= current year', function() {
+      expect(yearNotValid(currentYear)).to.be.false;
+      expect(yearNotValid(futureYear)).to.be.false;
+    });
+
+    it('returns true if input year < current year', function() {
+      expect(yearNotValid(pastYear)).to.be.true;
+    });
+  });
+
 });
