c0ea3fc86e55608b134c25ea119aebbd226e3962
felixtram <U315546@L76BT3G72.AD001.InfoFTPS.COM>
2017-03-23 09:31:11 -0600

    hooked up cvc

--- js/cvc-validation.js
+++ js/cvc-validation.js
@@ -17,8 +17,9 @@
  * @returns {Boolean}
  */
 function cvcNotNumeric(cvc){
-	var test = /^[0-9]+$/.test(cvc);
-	return !test;
+  // regex that matches 1 or non-numeric character 
+  var re = /\D+/;
+  return re.test(cvc); 
 }
 
 /**
@@ -28,7 +29,7 @@ function cvcNotNumeric(cvc){
  * @returns {Boolean}
  */
 function cvcShort(pan, cvc){
-	var type = detectCardType(pan);
+	var type = detectCardTypePartial(pan);
 	var length = getCvcLengthByCardType(type);
 	return underLength(cvc, Math.floor(length));
 }
@@ -40,7 +41,7 @@ function cvcShort(pan, cvc){
  * @returns {Boolean}
  */
 function cvcLong(pan, cvc){
-	var type = detectCardType(pan);
+	var type = detectCardTypePartial(pan);
 	var length = getCvcLengthByCardType(type);
 	return overLength(cvc, Math.ceil(length));
 }
@@ -97,7 +98,7 @@ function getCvcLengthByCardType(type){
 		return map[type];
 	}
 	else
-		return 4;
+		return 3.5;
 }
 
 /**
@@ -139,4 +140,35 @@ function cvcLengthAndNumericCheck(cvc, length){
 	var regex = new RegExp("^[0-9]{" + length + "}$");
 	var isnum = /^[0-9]+$/.test(cvc);
 	return isnum;
+}
+function detectCardTypePartial(pan) {
+  var re = {
+    //visa: 4
+    visa: /^4/,
+    //mastercard 51-55 and 22-27
+    mastercard: /^(5[1-5]|2[2-7])/, 
+    //amex 34, 37
+    amex: /^3[47]/, 
+    //jcb 35
+    jcb: /^35/, 
+    //unionpay 62, 88
+    unionpay: /^(62|88)/, 
+    //diners club 30, 36, 38, 39
+    diners: /^(30|36|38|39)/, 
+    //dankort 5019
+    dankort: /^5019/, 
+    //forbrugsforeningen 600
+    forbrugsforeningen: /^600/,
+    //maestro 5018, 502, 503, 506, 56, 58, 639, 6220, 67
+    maestro: /^(5018|502|503|506|56|58|639|6220|67)/, 
+    //discover: 60, 64, 65, 622
+    discover: /^(60|6[4,5]|622)/ 
+  };
+
+  for(var key in re) {
+    if(re[key].test(pan)) {
+        return key;
+    }
+  }
+  return undefined;
 }
--- js/validation-listeners.js
+++ js/validation-listeners.js
@@ -15,13 +15,13 @@ function panInitEvents() {
 
 }
 
-function panKeyPress(e) {
-    
+function panKeyPress(e) { 
   var charCode = e.which || e.keyCode;
 
   if (charCode > 31 && (charCode < 48 || charCode > 57)) {
       return false;
   }else{
+
     if (charCode == 8 || charCode == 9){
       return true;
     }
@@ -45,32 +45,7 @@ function panKeyPress(e) {
     iconSwitch(type);
 
     return false;
-    }
-
-
-  if (!isIneligible(keyPressed)) {
-      console.log("not eligible");
-    //get pan
-    var pan = $("#accountnumber").val();
-    //strip of white space
-    pan = stripSpaces(pan);
-
-    //add keypressed
-    pan = pan + keyPressed;
-
-    //update dom with formatted pan
-    $("#accountnumber").val(formatPan(pan));
-
-    //update card icon based on card type
-    var type = detectCardTypePartial(pan);
-    iconSwitch(type);
-
-    return false;
-  } else{
-    console.log("eligible")
-    return false;      
   }
-
 }
 
 function panBlur() {
@@ -79,6 +54,9 @@ function panBlur() {
 
   //strip spaces  
   pan = stripSpaces(pan);
+
+  //update dom with formatted pan
+  $("#accountnumber").val(formatPan(pan));
 
   //validate pan
   var panErrs = panValidations(pan);
@@ -134,4 +112,85 @@ function panPaste(e) {
   return false;
 }
 
+function cvcInitEvents() {
+  //blur event
+  $('#cvc').blur(function() {
+    cvcBlur();
+  })
+  //paste event
+  $('#cvc').bind("paste", cvcPaste);
+
+  //keypress
+  $("#cvc").keypress(cvcKeyPress);
+
+}
+
+function cvcKeyPress(e) { 
+  var charCode = e.which || e.keyCode;
+
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+  }else{
+
+    if (charCode == 8 || charCode == 9){
+      return true;
+    }
+    
+    var c = String.fromCharCode(charCode);
+
+    //get pan
+    var cvc = $("#cvc").val();
+
+    //strip of white space
+    cvc = stripSpaces(cvc);
+
+    //add keypressed
+    cvc = cvc + c;
+
+    //update dom with formatted pan
+    $("#cvc").val(cvc);
+
+    return false;
+  }
+}
+
+function cvcBlur() {
+  //get pan
+  var cvc = $("#cvc").val();
+  //strip spaces  
+  cvc = stripSpaces(cvc);
+  //get pan
+  var pan = $("#accountnumber").val();
+  //strip spaces  
+  pan = stripSpaces(pan);
+  //if cvv is not empty run cvv validations
+  var cvcErrs = cvcValidations(pan, cvc)
+
+  //update dom
+  cvcValidUi(cvcErrs);
+}
+
+function cvcPaste(e) {
+
+  //get pan
+  var cvc = e.originalEvent.clipboardData.getData('text');
+  console.log(cvc);
+  //update dom with cvc
+  $("#cvc").val(cvc);
+
+  //get pan
+  var pan = $("#accountnumber").val();
+  //strip spaces  
+  pan = stripSpaces(pan);
+  //if cvv is not empty run cvv validations
+  var cvcErrs = cvcValidations(pan, cvc)
+
+  //update dom
+  cvcValidUi(cvcErrs);
+  //do not paste text from clipboard
+  return false;
+}
+
+
 panInitEvents();
+cvcInitEvents();
